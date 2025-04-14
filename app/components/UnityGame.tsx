fbae7bf846e7b2179848bc3fab0355e97672e163
noac19 <142503548+NatiTsim@users.noreply.github.com>
2025-04-14 17:10:20 +0300

    Send And Received

--- app/components/UnityGame.tsx
+++ app/components/UnityGame.tsx
@@ -1,12 +1,47 @@
-import React from "react";
+import React, { useEffect, useState  } from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
+
+declare global {
+  interface Window {
+    onUnityMessage: (jsonString: string) => void;
+  }
+}
+
 export function UnityGame() {
-  const { unityProvider } = useUnityContext({
+  const { unityProvider, sendMessage, isLoaded } = useUnityContext({
     loaderUrl: "Build/SwarmUI.loader.js",
     dataUrl: "Build/SwarmUI.data",
     frameworkUrl: "Build/SwarmUI.framework.js",
     codeUrl: "Build/SwarmUI.wasm",
   });
+  const [jsonData, setJsonData] = useState<string | null>(null);
+  const [receivedFromUnity, setReceivedFromUnity] = useState<any>(null); // new
+
+  useEffect(() => {
+    window.onUnityMessage = function (jsonString) {
+      const parsed = JSON.parse(jsonString);
+      console.log("From Unity:", parsed.config);
+      console.log("From Unity:", parsed.indoor);
+    };
+  }, []);
+  // Fetch the JSON data from the public folder
+  useEffect(() => {
+    fetch("/Config.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setJsonData(JSON.stringify(data)); // Set the JSON data to the state
+      })
+      .catch((error) => console.error("Error loading JSON:", error));
+  }, []);
+  
+
+  useEffect(() => {
+    if (isLoaded && jsonData) {
+      console.log("Sending message to Unity with JSON:", jsonData);
+      sendMessage("ConfigManager", "ReceiveJson", jsonData);
+    }
+  }, [isLoaded, jsonData]);
+  
   return (
     <Unity
       unityProvider={unityProvider}
